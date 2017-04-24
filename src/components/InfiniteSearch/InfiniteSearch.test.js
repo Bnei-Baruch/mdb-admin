41b7e83bde7f420ba7cb9de210b63558914a4c35
yanivel <yaniv.elimor@gmail.com>
2017-04-24 05:46:32 +0300

    basic test

--- src/components/InfiniteSearch/InfiniteSearch.test.js
+++ src/components/InfiniteSearch/InfiniteSearch.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
-import { Column } from 'react-virtualized';
+import { Column, Table } from 'react-virtualized';
 import range from 'lodash/range';
 import InfiniteSearch from './InfiniteSearch';
 
@@ -19,27 +19,5 @@ describe('basic', () => {
 
     it('mounts without crashing', () => {
         mount(<InfiniteSearch columns={columns} resultItems={[]} params={{}} total={0} search={() => {}} />);
-    });
-
-    it('searchs without crashing', () => {
-        const search = (params = {}, isNewSearch = false, startIndex = 0, stopIndex = 10) => {
-            return new Promise(resolve => {
-                resolve(range(startIndex, stopIndex).map(index => ({ id: index })));
-            });
-        };
-
-        let total = 0;
-        let resultItems = [];
-
-        const wrapper = mount(<InfiniteSearch columns={columns} resultItems={[]} params={{}} total={total} search={search} />);
-
-        search().then(data => {
-            resultItems.push(data);
-            total = resultItems.length;
-        });
-
-        wrapper.setProps({ total, resultItems });
-
-
     });
 });
