976ccd9a0213492a5e785a36f3c7598e91936430
bbfsdev <bbfsdev@gmail.com>
2017-05-01 02:17:30 +0000

    Remove redundant import

--- src/components/Operation/Operation.js
+++ src/components/Operation/Operation.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import ObjectTable from '../ObjectTable/ObjectTable';
 import apiClient from '../../helpers/apiClient';
 import { OPERATION_TYPE_BY_ID } from '../../helpers/consts';
