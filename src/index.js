1f45a235f0bcb7cde2917b556925941ddea2d153
Edo Shor <edoshor@gmail.com>
2017-06-27 11:06:04 +0300

    dev - why-did-you-update for react optimizations

--- src/index.js
+++ src/index.js
@@ -1,74 +1,79 @@
-import "core-js/shim";
-import "regenerator-runtime/runtime";
-import React from "react";
-import ReactDOM from "react-dom";
-import {applyMiddleware, compose, createStore} from "redux";
-import createSagaMiddleware, {delay} from "redux-saga";
-import {put} from "redux-saga/effects";
-import {routerMiddleware as createRouterMiddleware} from "react-router-redux";
-import createHistory from "history/createBrowserHistory";
+import 'core-js/shim';
+import 'regenerator-runtime/runtime';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { applyMiddleware, compose, createStore } from 'redux';
+import createSagaMiddleware, { delay } from 'redux-saga';
+import { put } from 'redux-saga/effects';
+import { routerMiddleware as createRouterMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
-import reducer from "./redux";
-import {actions as system} from "./redux/modules/system";
-import allSagas from "./sagas";
-import sagaMonitor from "./sagas/sagaMonitor";
-import App from "./components/App/App";
-
+import reducer from './redux';
+import { actions as system } from './redux/modules/system';
+import allSagas from './sagas';
+import sagaMonitor from './sagas/sagaMonitor';
+import App from './components/App/App';
 
 //
 // Create redux store
 //
 const isProduction = process.env.NODE_ENV === 'production';
-const devToolsArePresent = typeof window === 'object' && typeof window.devToolsExtension !== 'undefined';
+
+if (!isProduction) {
+  const { whyDidYouUpdate } = require('why-did-you-update');
+  whyDidYouUpdate(React, { exclude: /Menu*|Dropdown*|List*|Button*|Message*|Link/ });
+}
+
+const devToolsArePresent    = typeof window === 'object' && typeof window.devToolsExtension !== 'undefined';
 const devToolsStoreEnhancer = () => !isProduction && devToolsArePresent ? window.devToolsExtension() : f => f;
 
-const sagaMiddlewareOptions = isProduction ? {} : {sagaMonitor};
-const sagaMiddleWare = createSagaMiddleware(sagaMiddlewareOptions);
+const sagaMiddlewareOptions = isProduction ? {} : { sagaMonitor };
+const sagaMiddleWare        = createSagaMiddleware(sagaMiddlewareOptions);
 
-const history = createHistory({
-    basename: isProduction ? '/admin/' : ''
+const history          = createHistory({
+  basename: isProduction ? '/admin/' : ''
 });
 const routerMiddleware = createRouterMiddleware(history);
 
 const store = createStore(reducer, {}, compose(
-    applyMiddleware(routerMiddleware, sagaMiddleWare),
-    devToolsStoreEnhancer()
+  applyMiddleware(routerMiddleware, sagaMiddleWare),
+  devToolsStoreEnhancer()
 ));
 
 // Render regardless of application's state. let App decide what to render.
 const appContainer = document.getElementById('root');
-ReactDOM.render(<App store={store} history={history}/>, appContainer);
+ReactDOM.render(<App store={store} history={history} />, appContainer);
 
 //
 // The main application
 //
-function * application() {
-    //
-    // Bootstrap the saga middleware with initial sagas
-    //
-    allSagas.forEach(saga => sagaMiddleWare.run(saga));
+function* application() {
+  //
+  // Bootstrap the saga middleware with initial sagas
+  //
+  allSagas.forEach(saga => sagaMiddleWare.run(saga));
 
-    //
-    // Tell everybody, that we're booting now
-    //
-    yield put(system.boot());
+  //
+  // Tell everybody, that we're booting now
+  //
+  yield put(system.boot());
 
-    // Future: Do Whatever bootstrap logic here
-    // Load configuration, load translations, etc...
+  // Future: Do Whatever bootstrap logic here
+  // Load configuration, load translations, etc...
 
-    // Future: Hydrate server state
-    // Deep merges state fetched from server with the state saved in the local storage
-    yield put(system.init({}));
+  // Future: Hydrate server state
+  // Deep merges state fetched from server with the state saved in the local storage
+  yield put(system.init({}));
 
-    //
-    // Just make sure that everybody does their initialization homework
-    //
-    yield delay(0);
+  //
+  // Just make sure that everybody does their initialization homework
+  //
+  yield delay(0);
 
-    //
-    // Inform everybody, that we're ready now
-    //
-    yield put(system.ready());
+  //
+  // Inform everybody, that we're ready now
+  //
+  yield put(system.ready());
 }
 
 sagaMiddleWare.run(application);
