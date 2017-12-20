780be269f9c866ea2ae927d868b864cf91655f8a
Edo Shor <edoshor@gmail.com>
2017-12-20 15:47:20 +0200

    Manage api authentication header in response to user availability

--- src/redux/modules/user.js
+++ src/redux/modules/user.js
@@ -2,6 +2,8 @@ import { handleActions } from 'redux-actions';
 import { USER_EXPIRED, USER_FOUND, USER_LOADED, USER_SIGNED_OUT } from 'redux-oidc';
 import { jws } from 'jsrsasign';
 
+import api from '../../helpers/apiClient';
+
 /* Reducer */
 
 const initialState = {
@@ -17,14 +19,24 @@ const onUser = (state, action) => {
   user.realm_access                                       = realm_access;  // eslint-disable-line camelcase
   user.resource_access                                    = resource_access; // eslint-disable-line camelcase
 
+  // Add 'Authorization' header to api client
+  api.defaults.headers.common.Authorization = `${user.token_type} ${user.access_token}`;
+
   return { user };
+};
+
+const onNoUser = () => {
+  // Remove Authorization header from api client
+  delete api.defaults.headers.common.Authorization;
+
+  return { user: null };
 };
 
 export const reducer = handleActions({
   [USER_LOADED]: onUser,
   [USER_FOUND]: onUser,
-  [USER_EXPIRED]: () => initialState,
-  [USER_SIGNED_OUT]: () => initialState,
+  [USER_EXPIRED]: onNoUser,
+  [USER_SIGNED_OUT]: onNoUser,
 }, initialState);
 
 /* Selectors */
