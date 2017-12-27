e39002fb8557fc732a961e754a1e09d7a36066c6
Edo Shor <edoshor@gmail.com>
2017-12-27 13:25:35 +0200

    Login Page - show indication of silent signin progress

--- src/components/FrontPage/LoginPage.js
+++ src/components/FrontPage/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Button, Grid, Image, Segment } from 'semantic-ui-react';
+import { Button, Grid, Header, Image, Segment } from 'semantic-ui-react';
 
 import * as shapes from '../shapes';
 import userManager from '../../helpers/userManager';
@@ -11,12 +11,18 @@ class LoginPage extends PureComponent {
     location: shapes.HistoryLocation.isRequired,
   };
 
+  state = {
+    signinSilent: true,
+  };
+
   componentDidMount() {
     // keep current location in state so we'll have it once auth is complete
     // and we're back in the app.
     // We need this to redirect the user to location he wanted in the first place.
     const { location } = this.props;
-    userManager.signinSilent({ state: JSON.stringify({ location }) });
+    userManager.signinSilent({ state: JSON.stringify({ location }) })
+      .then(user => this.setState({ signinSilent: false }))
+      .catch(err => this.setState({ signinSilent: false }));
   }
 
   handleLogin = (e) => {
@@ -27,6 +33,25 @@ class LoginPage extends PureComponent {
     // We need this to redirect the user to location he wanted in the first place.
     const { location } = this.props;
     userManager.signinRedirect({ state: JSON.stringify({ location }) });
+  };
+
+  renderSilentSigninStatus = () => {
+    const { signinSilent } = this.state;
+    if (!signinSilent) {
+      return null;
+    }
+
+    return (
+      <Header
+        textAlign="center"
+        size="tiny"
+        content="Checking session status"
+        icon={{
+          name: 'spinner',
+          loading: true
+        }}
+      />
+    );
   };
 
   render() {
@@ -56,6 +81,7 @@ class LoginPage extends PureComponent {
               <h3>Welcome to BB Archive admin app!</h3>
               <p>Please log in to continue</p>
               <Button primary size="huge" onClick={this.handleLogin}>Login</Button>
+              {this.renderSilentSigninStatus()}
             </Segment>
           </Grid.Column>
         </Grid>
