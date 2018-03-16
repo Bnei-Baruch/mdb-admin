015f96a31b2bba20b3ae0a40752dabaf125a6d30
davgur <davgur.ru@gmail.com>
2018-03-16 07:10:37 +0200

    order CCUs in association by position->type_id->created_at

--- src/components/Collection/tabs/associations/AssociationsTab.js
+++ src/components/Collection/tabs/associations/AssociationsTab.js
@@ -55,13 +55,22 @@ class AssociationsTab extends Component {
   }
 }
 
+function orderUnits(u1, u2) {
+  const d1 = new Date(u1.content_unit.created_at);
+  const d2 = new Date(u2.content_unit.created_at);
+  return d1 > d2 ? 1 : d1 === d2 ? 0 : -1;
+}
+
 const mapState = (state, ownProps) => {
   const { collection = EMPTY_OBJECT } = ownProps;
   const unitIDs                       = collection.content_units;
   const denormCCUs                    = units.denormCCUs(state.content_units);
-
+  const CCUs                          = unitIDs ? orderBy(denormCCUs(unitIDs).sort(orderUnits), ['position', 'content_unit.type_id']) : EMPTY_ARRAY;
+  if(unitIDs){
+    console.log(CCUs);
+  }
   return {
-    units: unitIDs ? orderBy(denormCCUs(unitIDs), 'position') : EMPTY_ARRAY,
+    units: CCUs,
     associatedCUIds: collection.content_units ?
       new Map(collection.content_units.map(x => [x.content_unit_id, true])) :
       new Map(),
