4253b52d5b5981ed4832c08bb7872f7d361c4014
davgur <davgur.ru@gmail.com>
2018-03-26 04:31:37 +0300

    rename action

--- src/components/ContentUnit/tabs/merge/filters/FreeText.js
+++ src/components/ContentUnit/tabs/merge/filters/FreeText.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { NS_COLLECTION_UNITS } from '../../../helpers/consts';
-import TextFilter from '../../Filters/TextFilter';
+import { NS_COLLECTION_UNITS } from '../../../../../helpers/consts';
+import TextFilter from '../../../../Filters/TextFilter';
 
 const FreeText = props => (
   <TextFilter
