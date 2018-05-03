3aa84d656fb3fe8ad272b8b31a7bf3f964dac1a4
davgur <davgur.ru@gmail.com>
2018-05-03 04:25:01 +0300

    add customs options

--- src/components/ContentUnits/filters/Others.js
+++ src/components/ContentUnits/filters/Others.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Grid, Header, Segment } from 'semantic-ui-react';
 
-import { NS_UNITS } from '../../../helpers/consts';
+import { NS_UNITS, CONTENT_UNIT_TYPES } from '../../../helpers/consts';
 import { ContentTypeFilter, PublishedFilter, SecureFilter } from '../../Filters/filterComponents';
 
 const Others = props => (
@@ -15,6 +15,7 @@ const Others = props => (
             namespace={NS_UNITS}
             name="content_type"
             onApply={props.onFilterApplication}
+            options={Array.from(Object.keys(CONTENT_UNIT_TYPES)).map(x => ({ key: x, value: x, text: x }))}
           />
         </Grid.Column>
         <Grid.Column width={4}>
