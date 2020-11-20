969a49d8a4974e4e58491ba2073076d368089346
davgur <davgur.ru@gmail.com>
2020-11-20 14:47:59 +0200

    try use customize-cra

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -1,4 +1,4 @@
-const { compose }           = require('react-app-rewired');
+const { compose }           = require('customize-cra');
 const rewireVendorSplitting = require('react-app-rewire-vendor-splitting');
 const rewireSilentRenew     = require('./OidcSilentRenew');
 
