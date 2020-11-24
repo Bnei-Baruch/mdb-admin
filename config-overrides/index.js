9367ad76a1d0d7526afa2f6b1fd2dc18ea3a15de
davgur <davgur.ru@gmail.com>
2020-11-24 16:47:36 +0200

    add customize-cra

--- config-overrides/index.js
+++ config-overrides/index.js
@@ -1,5 +1,4 @@
-const { compose }           = require('react-app-rewired');
-const rewireVendorSplitting = require('react-app-rewire-vendor-splitting');
+const { override }           = require('customize-cra');
+//const rewireVendorSplitting = require('react-app-rewire-vendor-splitting');
+module.exports = override(rewireSilentRenew);
 const rewireSilentRenew     = require('./OidcSilentRenew');
-
-module.exports = compose(rewireSilentRenew, rewireVendorSplitting);
