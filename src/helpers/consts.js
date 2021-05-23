8f3f5005375375c49dbf924120b64e4979a7447d
davgur <davgur.ru@gmail.com>
2021-05-23 12:08:51 +0300

    Ar adm 52 add cu type source (#105)
    * fix date range format bug
    
    * git ignore
    
    * add select series by tags
    
    * add link to CU on source
    
    * Ar adm 52 add cu type source (#87)
    
    * Ar adm 53 lesson series by tag (#84)
    
    * git ignore
    
    * add select series by tags
    
    * code review
    
    * Marging with staging (#85)
    
    * fix date range format bug
    
    * git ignore
    
    * add select series by tags
    
    * remove artefact
    
    * remove artefact
    
    * add link to CU on source
    
    * fix const
    
    * fix const
    
    * use i18n for cu from source
    
    * use uid instead id
    
    * bug with type_id
    
    * remove artefact
    
    * remove artefact

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -68,6 +68,7 @@ export const CT_BOOK                  = 'BOOK';
 export const CT_BLOG_POST             = 'BLOG_POST';
 export const CT_RESEARCH_MATERIAL     = 'RESEARCH_MATERIAL';
 export const CT_KTAIM_NIVCHARIM       = 'KTAIM_NIVCHARIM';
+export const CT_SOURCE                = 'SOURCE';
 
 export const CONTENT_TYPE_BY_ID = {
   1: CT_DAILY_LESSON,
@@ -110,6 +111,7 @@ export const CONTENT_TYPE_BY_ID = {
   44: CT_BLOG_POST,
   45: CT_RESEARCH_MATERIAL,
   46: CT_KTAIM_NIVCHARIM,
+  47: CT_SOURCE,
 };
 
 // Collection types
